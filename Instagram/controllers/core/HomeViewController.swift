7904b313a17abc2685acc86e12c385381757d081
robusta <mohamed.gabal@robustastudio.com>
2024-05-05 16:58:32 +0300

    first commit

---
+++ Instagram/controllers/core/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by robusta on 05/05/2024.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
